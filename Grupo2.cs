6e9dbbdafdca43a9ded6637d583ed2ff48caaeff
ZahiraParedes <zahiraparedesr@gmail.com>
2023-03-22 21:40:36 -0500

    Update Grupo2.cs

--- Grupo2.cs
+++ Grupo2.cs
@@ -4,14 +4,71 @@ using UnityEngine;
 
 public class Grupo2 : MonoBehaviour
 {
-    int x = Random.range(0, 10);
-    
-    if (x % 2 == 0)
+    void start()
     {
-        Debug.Log("Par");
+        numero1();
+        numero2();
+        numero3();
+        numero4();
+
     }
-    else
+
+    void numero1()
     {
+        int x = Random.range(0, 10);
+    
+        if (x % 2 == 0)
+        {
+            Debug.Log("Par");
+        }
+        else
+        {
         Debug.Log("Impar");
-    }    
+        }
+    }
+
+    void numero2()
+    {
+        int mes;
+        mes = random.range(1, 12);
+        switch(mes)
+        {
+            case 1:
+                Debug.Log("Enero");
+                break;
+            case 2:
+                Debug.Log("Febrero");
+                break;
+            case 3:
+                Debug.Log("Marzo");
+                break;
+            case 4:
+                Debug.Log("Abril");
+                break;
+            case 5:
+                Debug.Log("Mayo");
+                break;
+            case 6:
+                Debug.Log("Junio");
+                break;
+            case 7:
+                Debug.Log("Julio");
+                break;
+            case 8:
+                Debug.Log("Agosto");
+                break;
+            case 9:
+                Debug.Log("Septiembre");
+                break;
+            case 10:
+                Debug.Log("Octubre");
+                break;
+            case 11:
+                Debug.Log("Noviembre");
+                break;
+            case 12:
+                Debug.Log("Diciembre");
+                break;
+        }
+    }
 }
