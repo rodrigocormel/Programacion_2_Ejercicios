f87e279dea52dbd50c415cecdb8d02c250594f67
ZahiraParedes <zahiraparedesr@gmail.com>
2023-03-22 21:29:12 -0500

    if

--- Grupo2.cs
+++ Grupo2.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Grupo2 : MonoBehaviour
 {
+    int x = Random.range(0, 10);
     
+    if (x % 2 == 0)
+    {
+        Debug.Log("Par");
+    }
+    else
+    {
+        Debug.Log("Impar");
+    }    
 }
